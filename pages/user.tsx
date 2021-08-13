166f34deea0da90910eaee243794c7eb217e07d3
joseph tran <anhtt@a-livesystems.com>
2021-08-13 11:01:14 +0700

    useRef

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 export default function User() {
   useMemo(() => {
@@ -28,9 +28,31 @@ export default function User() {
     setVisible(!visible)
   }, [visible])
 
+  const handleClickSearch = (() => { })
+  const inputEl = useRef(null);
+
+  const fileEl = useRef(null);
+  const handleUploadImage = (() => {
+    fileEl.current.click()
+
+  })
+
   return (
-    <>
+    <div style={{ margin: 30, lineHeight: 2 }}>
       <h1>Hello User {fullName}</h1>
+      <div>
+        <input type="text" ref={inputEl} />
+        <button onClick={handleClickSearch}>Search</button>
+      </div>
+      <div style={{ margin: '10px 0' }}>
+        <input style={{ visibility: 'hidden', width: 0, height: 0 }} type="file" ref={fileEl} />
+        <button style={{
+          width: 100, height: 40,
+          background: '#000',
+          border: 0,
+          color: '#fff'
+        }} onClick={handleUploadImage}>Upload image</button>
+      </div>
       <button onClick={handleVisible}>Toggle visible</button>
       <button onClick={handleCreateCounter}>Counter add</button>
       {visible && (
@@ -39,6 +61,6 @@ export default function User() {
         </>
       )}
 
-    </>
+    </div>
   )
 }
